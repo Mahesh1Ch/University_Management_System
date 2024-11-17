5c6bf3498184e5a0ee0bd562379efd16ae03f2d3
mahes_cmozf3b <maheswarreddychagam555@gamil.com>
2024-11-17 11:39:34 +0530

    updating the api

--- university-management/identity-service/src/main/java/com/identity/identity_service/controller/AdminLoginController.java
+++ university-management/identity-service/src/main/java/com/identity/identity_service/controller/AdminLoginController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/adminlogin")
 public class AdminLoginController {
@@ -18,18 +21,31 @@ public class AdminLoginController {
     }
 
     @PostMapping("/otpgeneration")
-    public ResponseEntity<String>  generateLoginOtp(@RequestParam String phone) {
+    public ResponseEntity<Map<String,String>>  generateLoginOtp(@RequestParam String phone) {
         String responseMessage = adminLoginService.generateLoginOtp(phone);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", responseMessage);  // The message will be "OTP sent to 9999999990"
+        response.put("status", "success");
 
-        return ResponseEntity.ok(responseMessage);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/otpvalidation")
-    public ResponseEntity<Admin> validateLoginOtp(@RequestParam String phone,@RequestParam String otpInput)
+    public ResponseEntity<Map<String,Object>> validateLoginOtp(@RequestParam String phone,@RequestParam String otpInput)
     {
         Admin admin = adminLoginService.validateLoginOtp(phone,otpInput);
 
-        return ResponseEntity.ok(admin);
+        Map<String, Object> response = new HashMap<>();
+
+        if (admin != null) {
+            response.put("status", "success");  // Adding the status field
+            response.put("data", admin);        // Adding the admin data
+        } else {
+            response.put("status", "error");
+            response.put("message", "Invalid OTP or registration failed");
+        }
+
+        return ResponseEntity.ok(response);
     }
 
 
